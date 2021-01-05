16b350a8c52059ac8fc0bc7208b8318c72405d79
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-05 13:14:28 +0100

    Navigation (style) created commit

--- react-cv/src/pages/Portfolio.js
+++ react-cv/src/pages/Portfolio.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navigation from '../components/Navigation';
 
 const Portfolio = () => {
     return (
         <div>
-            <h3>portfolio</h3>
+            <Navigation />
         </div>
     );
 };
