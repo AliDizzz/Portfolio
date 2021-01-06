b153c0bb183286f71f1d1045a4728a14dfdbac6d
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-06 14:33:40 +0100

    Page Knowledges ProgressBar experience page commit

--- react-cv/src/components/knowledges/Experience.js
+++ react-cv/src/components/knowledges/Experience.js
@@ -2,9 +2,28 @@ import React from 'react';
 
 const Experience = () => {
     return (
-        <div>
-            Exp
+        <div className="experience">
+            <h3>Expérience</h3>
+            <div className="exp-1">
+                <h4>Développeur web </h4>
+                <h5>2019-2021</h5>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Cum sed nisi laboriosam in velit rem delectus. Culpa, delectus veritatis 
+                    totam incidunt nobis ipsam ipsum amet quos odio quo, unde est.
+                </p>
+            </div>
+            <div className="exp-2">
+                <h4>Développeur web </h4>
+                <h5>2019-2021</h5>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Cum sed nisi laboriosam in velit rem delectus. Culpa, delectus veritatis 
+                    totam incidunt nobis ipsam ipsum amet quos odio quo, unde est.
+                </p>
+            </div>
         </div>
+        
     );
 };
 
