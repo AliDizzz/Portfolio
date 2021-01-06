53ac5f1a82589caf225d061b440c766bff2a4d63
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-06 14:59:32 +0100

    Page Knowledges ProgressBar icons added commit

--- react-cv/src/components/knowledges/Hobbies.js
+++ react-cv/src/components/knowledges/Hobbies.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 const Hobbies = () => {
     return (
-        <div>
-            hobbies
+        <div className="hobbies">
+            <h3>Intérêts</h3>
+            <ul>
+                <li className="hobby">
+                    <i className="fas fa-running"></i>
+                    <span>Course à pied</span>
+                </li>
+                <li className="hobby">
+                    <i className="fas fa-futbol"></i>
+                    <span>Football</span>
+                </li>
+                <li className="hobby">
+                    <i className="fas fa-laptop-code"></i>
+                    <span>Coder</span>
+                </li>
+                <li className="hobby">
+                    <i className="fas fa-dumbbell"></i>
+                    <span>Musculation</span>
+                </li>
+                <li className="hobby">
+                    <i className="fas fa-rocket"></i>
+                    <span>Espace</span>
+                </li>
+            </ul>
         </div>
     );
 };
--- react-cv/src/components/knowledges/OtherSkills.js
+++ react-cv/src/components/knowledges/OtherSkills.js
@@ -2,8 +2,49 @@ import React from 'react';
 
 const OtherSkills = () => {
     return (
-        <div>
-            other skill
+        <div className="otherSkills">
+            <h3>Autres compétences</h3>
+            <div className="list">
+
+                <ul>
+                    <li>
+                        <i className="fas fa-check-square"></i>
+                        Anglais technique
+                    </li>
+                    <li>
+                        <i className="fas fa-check-square"></i>
+                        Github
+                    </li>                    
+                    <li>
+                        <i className="fas fa-check-square"></i>
+                        SEO
+                    </li>                    
+                    <li>
+                        <i className="fas fa-check-square"></i>
+                        Méthodes agiles
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <i className="fas fa-check-square"></i>
+                        Commercial
+                    </li>
+                    <li>
+                        <i className="fas fa-check-square"></i>
+                        Autonome
+                    </li>                    
+                    <li>
+                        <i className="fas fa-check-square"></i>
+                        Force de persuasion - Enthousiasme
+                    </li>                    
+                    <li>
+                        <i className="fas fa-check-square"></i>
+                        Aptitudes relationnelles
+                    </li>
+                </ul>
+
+            </div>
         </div>
     );
 };
