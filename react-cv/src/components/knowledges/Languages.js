6c110b8edabf2d110032cc8c6fd4bbf0c9577eec
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-06 14:01:03 +0100

    Page Knowledges ProgressBar setting commit

--- react-cv/src/components/knowledges/Languages.js
+++ react-cv/src/components/knowledges/Languages.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ProgressBar from "./ProgressBar";
 
-const Languages = () => {
-    return (
-        <div>
-            languages
-        </div>
-    );
-};
+class Languages extends Component {
+    state = {
+        languages: [
+            {id: 1, value: "Javascript", xp:0.6},
+            {id: 2, value: "Css", xp:0.6},
+            {id: 3, value: "Php", xp:0.3},
+            {id: 4, value: "Pyton", xp:0.1}
+        ],
+        frameworks: [
+            {id: 1, value: "React", xp:0.6},
+            {id: 1, value: "Symfony", xp:0.3},
+            {id: 2, value: "Bootstrap", xp:0.6},
+            {id: 2, value: "Tailwind", xp:0.2},
+            {id: 3, value: "Sass", xp:0.2},
+            {id: 4, value: "Material UI", xp:0.2}
+        ]
+    }
+
+
+    render() {
+        let {languages, frameworks} = this.state;  //Quand on va dire languages ou framework, ce sera l'equivalent de this.state.language
+        
+        return (
+            <div className="languagesFrameworks">
+                <ProgressBar />
+                <ProgressBar />
+            </div>
+        );
+    }
+}
 
 export default Languages;
