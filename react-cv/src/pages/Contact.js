866d1362536e5aa835f71940801b9d62272376dc
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-07 13:43:35 +0100

    Page Contact commit

--- react-cv/src/pages/Contact.js
+++ react-cv/src/pages/Contact.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const Contact = () => {
     return (
-        <div>
+        <div className="contact">
             <Navigation />
+            <div className="contactContent">
+                <div className="header"></div>
+                <div className="contactBox">
+                    <h1>Contactez-moi</h1>
+                    <ul>
+                        <li>
+                            <i className="fas fa-map-marker-alt"></i>
+                            <span>Saint-Maur-Des-Fosses</span>
+                        </li>
+                        <li>
+                            <i className="fas fa-map-marker-alt"></i>
+                            <CopyToClipboard text="0773484244">
+                                <span 
+                                    className="clickInput"
+                                    onClick={() => { alert('Téléphone bien copié !');}}
+                                >
+                                    07 73 48 42 44 
+                                </span>
+                            </CopyToClipboard>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
