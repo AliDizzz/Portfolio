3a4001aa8c80855cce0cf87ea817316c6a36a8fa
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-07 09:27:32 +0100

    Page Portfolio commit

---
+++ react-cv/src/components/portfolio/Project.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Project extends Component {
+
+
+
+    render() {
+        let {name, languagesIcons, source, info, picture} = this.props.item;
+
+        return (
+            <div className="project">
+                <div className="icons">
+                    {languagesIcons.map(icon =>
+                        <i className={icon} key={icon}></i>
+                        )}
+                </div>
+            </div>
+        )
+    }
+}
