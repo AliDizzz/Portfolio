cf4b5f036dd86d87be575fb5f190b2d9d3393457
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-08 14:28:21 +0100

    pp resize commit

--- react-cv/src/components/Navigation.js
+++ react-cv/src/components/Navigation.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 
-
 const Navigation = () => {
 
     return (
         <div className="sidebar">
             <div className="id">
                 <div className="idContent">
-                    <img src="./media/billGates.jpg" alt="profil-pic"/>
+                    <img src="./media/pp-moi2.jpg" alt="profil-pic" />
                     <h3>Ali Yildiz</h3>
                 </div>
             </div>
