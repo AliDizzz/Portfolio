abf595734544f8f177c725cf04e51222afc752a9
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-10 17:55:54 +0100

    project 1 update commit

--- react-cv/src/data/portfolioData.js
+++ react-cv/src/data/portfolioData.js
@@ -1,12 +1,12 @@
 export const portfolioData = [
   {
     id: 1,
-    name: 'ipsum.com',
-    languages: ['javascript','css'],
-    languagesIcons: ['fab fa-js','fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project1.JPG'
+    name: 'Marvel Quiz',
+    languages: ['javascript','css', 'react'],
+    languagesIcons: ['fab fa-js','fab fa-css3-alt','fab fa-react'],
+    source: 'https://marvel-quiz-5a784.web.app/',
+    info: 'Application mobile en React.js avec une gestion de la DB par Firebase ainsi qu\'une manipulation de l\'API Marvel ',
+    picture: './media/project1.png'
   },
   {
     id: 2,
