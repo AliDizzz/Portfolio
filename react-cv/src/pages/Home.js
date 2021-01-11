b99bd3ee0777633b54d8c4dfbada827f67d1cd33
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-01-11 23:38:07 +0100

    first build commit

--- react-cv/src/pages/Home.js
+++ react-cv/src/pages/Home.js
@@ -1,19 +1,25 @@
 import React from 'react';
+
 import Navigation from '../components/Navigation';
 
 const Home = () => {
+
+    
+
     return (
         <div className="home">
             <Navigation />
             <div className="homeContent">
                 <div className="content">
-                    <h1>DEVELOPPEUR Junior</h1>
+                    <h1 >Développeur</h1>
+                    <h1>Junior</h1>
                     <h2>Web et Web Mobile</h2>
                     <div className="pdf">
                         <a href="./media/DeveloppeurWeb-Yildiz.pdf" target="_blank">Télécharger CV</a>
                     </div>
                 </div>
             </div>
+
         </div>
     );
 };
